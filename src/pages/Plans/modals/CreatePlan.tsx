244cd6a99b55cf289a25438e7947e2da6f3c3b3e
David Johnson <davemoses3@gmail.com>
2023-12-02 01:28:00 +0100

    create plan

--- src/pages/Plans/modals/CreatePlan.tsx
+++ src/pages/Plans/modals/CreatePlan.tsx
@@ -14,9 +14,10 @@ interface IForms {
   annual_rent: { amount: string; currency: string };
   amount: { amount: string; currency: string };
   frequency: 'daily' | 'weekly' | 'monthly';
-  automation_status: string;
+  automation_status: boolean;
   rent_due: string;
   target_date: string;
+  duration: boolean;
 }
 const CreatePlan = () => {
   const {
@@ -86,7 +87,71 @@ const CreatePlan = () => {
             }}
             errors={errors}
           />
+
+          <FormCheckbox<IForms>
+            id="duration"
+            name="duration"
+            label={
+              <CustomInputLabel
+                title="Plan duration"
+                subtitle="Include plan duration a start and end date."
+              />
+            }
+            type="checkbox"
+            className="!mt-6 w-full"
+            wrapperClass="!flex !items-start"
+            register={register}
+            rules={{
+              required: true,
+            }}
+            errors={errors}
+          />
         </div>
+        {watch('duration') && (
+          <div className="border-b px-4 py-6 md:px-8">
+            <h3 className="mb-6 text-lg font-semibold text-main-header md:text-2xl">
+              Savings duration
+            </h3>
+
+            <FormInput<IForms>
+              id="plan_name"
+              type="date"
+              name="plan_name"
+              className="my-4"
+              placeholder="Plan name"
+              label={
+                <CustomInputLabel
+                  title="Start Date"
+                  subtitle="Specify the start date for your plan payment."
+                />
+              }
+              register={register}
+              rules={{
+                required: true,
+              }}
+              errors={errors}
+            />
+
+            <FormInput<IForms>
+              id="plan_name"
+              type="date"
+              name="plan_name"
+              className="my-4"
+              placeholder="Rent amount"
+              label={
+                <CustomInputLabel
+                  title="End Date"
+                  subtitle="When do you aim to reach your plan savings goal"
+                />
+              }
+              register={register}
+              rules={{
+                required: true,
+              }}
+              errors={errors}
+            />
+          </div>
+        )}
         <div className="border-b px-4 py-6 md:px-8">
           <h3 className="mb-6 text-lg font-semibold text-main-header md:text-2xl">
             Savings Plan and Automation
@@ -187,50 +252,6 @@ const CreatePlan = () => {
           />
         </div>
 
-        <div className="border-b px-4 py-6 md:px-8">
-          <h3 className="mb-6 text-lg font-semibold text-main-header md:text-2xl">
-            Rent Payment Schedule
-          </h3>
-
-          <FormInput<IForms>
-            id="plan_name"
-            type="date"
-            name="plan_name"
-            className="my-4"
-            placeholder="Plan name"
-            label={
-              <CustomInputLabel
-                title="Rent Due Date"
-                subtitle="Specify the due date for your upcoming rent payment."
-              />
-            }
-            register={register}
-            rules={{
-              required: true,
-            }}
-            errors={errors}
-          />
-
-          <FormInput<IForms>
-            id="plan_name"
-            type="date"
-            name="plan_name"
-            className="my-4"
-            placeholder="Rent amount"
-            label={
-              <CustomInputLabel
-                title="Target Date"
-                subtitle="When do you aim to reach your rent savings goal"
-              />
-            }
-            register={register}
-            rules={{
-              required: true,
-            }}
-            errors={errors}
-          />
-        </div>
-
         <div className="px-4 py-6 md:px-8">
           <div className="mb-6">
             <h3 className="text-lg font-semibold text-main-header md:text-2xl">
@@ -297,7 +318,9 @@ const CreatePlan = () => {
             errors={errors}
           />
 
-          <Button className='mt-6' fullWidth>Create plan</Button>
+          <Button className="mt-6" fullWidth>
+            Create plan
+          </Button>
         </div>
       </form>
     </div>
